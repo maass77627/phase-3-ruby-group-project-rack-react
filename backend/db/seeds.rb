b5d62dde3b2fcf828e5c700bc1a8a634a8052700
kelly_maass <kmaass77627@gmail.com>
2025-04-18 20:09:49 -0500

    adding food

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,3 +2,9 @@
 User.create(firstname: "Jill", lastname: "Smith", email: "Jillsmith@gmail.com", age: 40, hometown: "Austin")
 
 Food.create(name: "banana", description: "healthy, yellow, fruit", category: "fruit",  user_id: 1, image: "banana2.png")
+
+Food.create(name: "apple", description: "healthy, red, fruit", category: "fruit",  user_id: 1, image: "apple2.png")
+
+Food.create(name: "rice", description: "healthy, white", category: "grain",  user_id: 1, image: "rice.png")
+
+Food.create(name: "burger", description: "unhealthy", category: "junk food",  user_id: 1, image: "burger.png")
