42312d550bd880cbc8142ca8579cebd84d8fab9f
kelly_maass <kmaass77627@gmail.com>
2025-04-28 20:03:52 -0500

    done

--- backend/app/food.rb
+++ backend/app/food.rb
@@ -3,16 +3,7 @@ class Food < ActiveRecord::Base
 
 belongs_to :user
 
-# attr_accessor :name, :description, :category, :calories
 
-#  def initialize(name, description, category, calories)
-#      @name = name
-#      @description = description
-#      @category = category
-#      @calories = calories
-
-
-#  end
 
 
 end
