19c089115ffc5b35722b8875d3879923ad43e980
kelly_maass <kmaass77627@gmail.com>
2025-04-28 20:04:20 -0500

    done

--- frontend/projectthree/src/Data.js
+++ frontend/projectthree/src/Data.js
@@ -1,34 +1,82 @@
 import React from "react";
 //  import { data } from "react-router-dom";
 import { PieChart, Pie, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
 
 
 
 function Data() {
 
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+    
+
     const data01 = [
-        { name: 'Group A', value: 400 },
-        { name: 'Group B', value: 300 },
-        { name: 'Group C', value: 300 },
-        { name: 'Group D', value: 200 },
-        { name: 'Group E', value: 278 },
-        { name: 'Group F', value: 189 },
+        { name: 'Vegetable', value: 400 },
+        { name: 'Fruit', value: 300 },
+        { name: 'Protein', value: 300 },
+        { name: 'Grain', value: 200 },
+        { name: 'Dairy', value: 278 },
+        { name: 'Fat', value: 189 },
       ];
       
       const data02 = [
         { name: 'Group A', value: 2400 },
         { name: 'Group B', value: 4567 },
-        { name: 'Group C', value: 1398 },
-        { name: 'Group D', value: 9800 },
-        { name: 'Group E', value: 3908 },
-        { name: 'Group F', value: 4800 },
+        // { name: 'Group C', value: 1398 },
+        // { name: 'Group D', value: 9800 },
+        // { name: 'Group E', value: 3908 },
+        // { name: 'Group F', value: 4800 },
       ];
 
    
     return(
         <div id="data">
-             <PieChart width={800} height={800}>
+             <PieChart id="pie" width={600} height={600}>
           <Pie
+          
             dataKey="value"
             isAnimationActive={false}
             data={data01}
@@ -41,6 +89,27 @@ function Data() {
           <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
           <Tooltip />
         </PieChart>
+        
+        <BarChart
+        id="bar"
+          width={600}
+          height={500}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+          <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+        </BarChart>
 
 
             
