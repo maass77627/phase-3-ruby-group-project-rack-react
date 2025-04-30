f6ccf86bc82e95297322934dad4993bd130ca3ee
kelly_maass <kmaass77627@gmail.com>
2025-04-30 17:32:26 -0500

    done

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -8,18 +8,17 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/foods/) && req.get?
-      # return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
       resp.write Food.all.to_json
 
     elsif req.path.match(/foods/) && req.post?
        data = JSON.parse req.body.read
-      food = Food.create(name: data["name"], description: data["description"], category: data["category"], calories: data["calories"], image: data["image"])
+       food = Food.create(name: data["name"], description: data["description"], category: data["category"], calories: data["calories"], image: data["image"])
     
        resp.write(food.to_json)
-      #  return [200, { 'Content-Type' => 'application/json' }, [ {:food => food}.to_json ]]   
+      
     elsif req.delete?
       puts "recieved request"
-     id = req.path.split("/foods/").last
+      id = req.path.split("/foods/").last
       Food.find(id).delete
     else
       resp.write "Path Not Found"
